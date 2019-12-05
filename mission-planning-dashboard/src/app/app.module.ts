a2c2c7cc10568be74f871b6800185d86a23f92cb
Denise Williams <denise.marie.84@gmail.com>
2019-12-04 18:42:56 -0600

    updated

--- mission-planning-dashboard/src/app/app.module.ts
+++ mission-planning-dashboard/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CrewComponent } from './crew/crew.component';
+import { ExperimentsComponent } from './experiments/experiments.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CrewComponent,
+    ExperimentsComponent
   ],
   imports: [
     BrowserModule
