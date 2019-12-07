ecb62192eab604756805a0edb739403662dd93a6
Denise Williams <denise.marie.84@gmail.com>
2019-12-07 13:19:04 -0600

    Finishing up Studio Uno

--- mission-planning-dashboard/src/app/app.module.ts
+++ mission-planning-dashboard/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { CrewComponent } from './crew/crew.component';
 import { ExperimentsComponent } from './experiments/experiments.component';
+import { EquipmentComponent } from './equipment/equipment.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     CrewComponent,
-    ExperimentsComponent
+    ExperimentsComponent,
+    EquipmentComponent
   ],
   imports: [
     BrowserModule
